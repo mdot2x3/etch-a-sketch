0c08b0896b8a8a2a983c417326fb6361590b810d
mdot2x3 <mdot2x3@gmail.com>
2024-05-24 19:07:48 -0700

    feat: Add button functionality, rearrange code
    when button is pressed the grid is removed.
    
    moved code around to build order chronology,
    pulled code chunks out of functions in cases
    where they should be their own separate
    functions.

--- javascript.js
+++ javascript.js
@@ -2,26 +2,9 @@ const body = document.querySelector('body');
 const grid = document.querySelector('.container');
 let box = [];
 
-// create button via DOM
-function gridButton () {
-    const buttonCont = document.createElement('div');
-    buttonCont.classList.add('buttonCont');
-    const button = document.createElement('button');
-    buttonCont.appendChild(button);
-    body.insertBefore(buttonCont, grid);
-
-    // event handler for button user can use to set grid size
-    button.addEventListener('click', () => {
-    let userChoice = prompt("Building grid... Please enter the number of squares per side(1-100 are valid)(default set to 16).", 16);
-    while (userChoice < 1 || userChoice > 100) {
-        userChoice = prompt("That is not a valid value. Please enter a number between 1 and 100. (default is set to 16).", 16);
-    };
-})
-}
-
 // create new div elements
-function boxMaker () {
-    for (let i = 0; i < (16 * 16); i++) {
+function boxMaker (num) {
+    for (let i = 0; i < (num * num); i++) {
         const oneBox = document.createElement('div');
         oneBox.classList.add('box');
         box.push(oneBox);
@@ -40,9 +23,43 @@ grid.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = '#FF10F0';
 });
 
-gridButton();
-boxMaker();
+// create button via DOM
+function gridButton () {
+    const buttonCont = document.createElement('div');
+    buttonCont.classList.add('buttonCont');
+    const button = document.createElement('button');
+    buttonCont.appendChild(button);
+    body.insertBefore(buttonCont, grid);
+
+    buttonPrompt(button);
+}
+
+// event handler for button user can use to set grid size
+function buttonPrompt (button) {
+    button.addEventListener('click', () => {
+        let userChoice = prompt("Building grid... Please enter the number of squares per side(1-100 are valid)(default set to 16).", 16);
+        
+        while (userChoice < 1 || userChoice > 100) {
+            userChoice = prompt("That is not a valid value. Please enter a number between 1 and 100. (default is set to 16).", 16);
+        };
+    
+        rebuildGrid(userChoice);
+
+    });
+}
+
+// after user input received delete current grid and build a new one
+function rebuildGrid (userChoice) {
+    body.remove(grid);
+    // create new grid here
+    boxMaker(userChoice);
+    printGrid();
+}
+
+boxMaker(16);
 printGrid();
+gridButton();
+
 
 
 // // create new div elements
